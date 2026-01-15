adf53e0046143f542d660b214fdeb0e46613ed42
j66357791-lab <j66357791@gmail.com>
2026-01-15 18:15:49 +0800

    Add files via upload

---
+++ invest-platform/frontend/src/store/authStore.js
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export const useAuthStore = create(
+  persist(
+    (set, get) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+      
+      login: (user, token) => {
+        set({ user, token, isAuthenticated: true });
+      },
+      
+      logout: () => {
+        set({ user: null, token: null, isAuthenticated: false });
+      },
+      
+      updateUser: (userData) => {
+        set((state) => ({
+          user: { ...state.user, ...userData }
+        }));
+      },
+      
+      getAuthHeaders: () => {
+        const { token } = get();
+        return {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        };
+      }
+    }),
+    {
+      name: 'auth-storage'
+    }
+  )
+);
